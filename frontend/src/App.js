2b96411d0832816e3893f397a4993fec4f2c075f
Ivan Yakovenko <32681800+meyakovenkoj@users.noreply.github.com>
2021-02-22 18:36:42 +0300

    REC-2 refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,44 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Recipes from './Recipes';
+import React, { Component } from "react";
+import axios from "axios";
+import Recipes from "./Recipes";
 
 class App extends Component {
-
   state = {
-    name: '',
-    description: '',
-    image: null
+    name: "",
+    description: "",
+    image: null,
   };
 
   handleChange = (e) => {
     this.setState({
-      [e.target.id]: e.target.value
-    })
+      [e.target.id]: e.target.value,
+    });
   };
 
   handleImageChange = (e) => {
     this.setState({
-      image: e.target.files[0]
-    })
+      image: e.target.files[0],
+    });
   };
 
   handleSubmit = (e) => {
     e.preventDefault();
     console.log(this.state);
     let form_data = new FormData();
-    form_data.append('image', this.state.image, this.state.image.name);
-    form_data.append('name', this.state.name);
-    form_data.append('description', this.state.description);
-    let url = 'http://localhost:8000/api/recipe/';
-    axios.post(url, form_data, {
-      headers: {
-        'content-type': 'multipart/form-data'
-      }
-    })
-        .then(res => {
-          console.log(res.data);
-        })
-        .catch(err => console.log(err))
+    form_data.append("image", this.state.image, this.state.image.name);
+    form_data.append("name", this.state.name);
+    form_data.append("description", this.state.description);
+    let url = "http://localhost:8000/api/recipe/";
+    axios
+      .post(url, form_data, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
   };
 
   render() {
@@ -46,20 +46,37 @@ class App extends Component {
       <div className="App">
         <form onSubmit={this.handleSubmit}>
           <p>
-            <input type="text" placeholder='Name' id='name' value={this.state.name} onChange={this.handleChange} required/>
+            <input
+              type="text"
+              placeholder="Name"
+              id="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
           </p>
           <p>
-            <input type="text" placeholder='Description' id='description' value={this.state.description} onChange={this.handleChange} required/>
-
+            <input
+              type="text"
+              placeholder="Description"
+              id="description"
+              value={this.state.description}
+              onChange={this.handleChange}
+              required
+            />
           </p>
           <p>
-            <input type="file"
-                   id="image"
-                   accept="image/png, image/jpeg"  onChange={this.handleImageChange} required/>
+            <input
+              type="file"
+              id="image"
+              accept="image/png, image/jpeg"
+              onChange={this.handleImageChange}
+              required
+            />
           </p>
-          <input type="submit"/>
+          <input type="submit" />
         </form>
-        <Recipes/>
+        <Recipes />
       </div>
     );
   }
--- frontend/src/Recipes.js
+++ frontend/src/Recipes.js
@@ -1,66 +1,67 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Recipes extends Component {
-
   state = {
-    recipes: []
+    recipes: [],
   };
 
-//   handleChange = (e) => {
-//     this.setState({
-//       [e.target.id]: e.target.value
-//     })
-//   };
+  componentDidMount() {
+    let url = "http://localhost:8000/api/recipe/";
+    axios
+      .get(url, {})
+      .then((res) => {
+        if (Array.isArray(res.data.Recipes)) {
+          this.setState({
+            recipes: res.data.Recipes,
+          });
+        }
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  }
 
-//   handleImageChange = (e) => {
-//     this.setState({
-//       image: e.target.files[0]
-//     })
-//   };
-    componentDidMount(){
-        let url = 'http://localhost:8000/api/recipe/';
-    axios.get(url, {
-    })
-        .then(res => {
-            this.setState({
-                      recipes: res.data.Recipes
-                    });
-          console.log(res.data);
-        })
-        .catch(err => console.log(err))
-    }
-
-    handleDelete(itemId){
-        let url = 'http://localhost:8000/api/recipe/';
-        axios.delete(url, { params: { id: itemId } 
-        })
-            .then(res => {
-                this.setState({
-                          recipes: this.state.recipes.filter(item => item.id !== itemId)
-                        });
-            })
-            .catch(err => console.log(err))
-    }
-
-
+  handleDelete(itemId) {
+    let url = "http://localhost:8000/api/recipe/";
+    axios
+      .delete(url, { params: { id: itemId } })
+      .then((res) => {
+        this.setState({
+          recipes: this.state.recipes.filter((item) => item.id !== itemId),
+        });
+      })
+      .catch((err) => console.log(err));
+  }
 
   render() {
+    console.log(this.state.recipes);
     return (
       <div className="Recipes">
-          <ul>
-       {this.state.recipes.map(recipe => {
-          return (
-              <div>
-                  <button onClick={() => {this.handleDelete(recipe.id)}}>Delete</button>
-            <li key={recipe.id}>
-              {recipe.name} - {recipe.description}
-            </li>
-            <img src={'http://localhost:8000/media/'+recipe.image} width="350"
-   height="250" alt=""/>
-</div>
-          );
-        })}
+        <ul>
+          {this.state.recipes?.map((recipe) => {
+            return (
+              <div key={recipe.id}>
+                <li>
+                  <p>
+                    {recipe.name} - {recipe.description}
+                  </p>
+                  <button
+                    onClick={() => {
+                      this.handleDelete(recipe.id);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </li>
+                <img
+                  src={"http://localhost:8000/media/" + recipe.image}
+                  width="350"
+                  height="250"
+                  alt=""
+                />
+              </div>
+            );
+          })}
         </ul>
       </div>
     );
